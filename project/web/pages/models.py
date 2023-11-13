fff7dfb12ab58098eec1b1a06b7eecf44f970f97
RazanAlbassam <147523554+RazanAlbassam@users.noreply.github.com>
2023-11-13 13:57:09 +0300

    Admin

--- project/web/pages/models.py
+++ project/web/pages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+
+class product (models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', default='null')
+
+
+    def __str__(self):
+        return self.name
+    
+   
+
+
+
